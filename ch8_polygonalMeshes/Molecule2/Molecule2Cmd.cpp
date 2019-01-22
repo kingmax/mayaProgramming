0126d9c9b1ee12acc2f5b74ca608877356d47058
Jason <kingmax_res@163.com>
2019-01-22 23:57:17 +0800

    + Molecule2.sln

---
+++ ch8_polygonalMeshes/Molecule2/Molecule2Cmd.cpp
@@ -0,0 +1,15 @@
+#include <maya/MPxCommand.h>
+#include <maya/MStatus.h>
+#include <maya/MSyntax.h>
+#include <maya/MDistance.h>
+#include <maya/MDagPath.h>
+#include <maya/MDagPathArray.h>
+#include <maya/MArgList.h>
+#include <maya/MArgDatabase.h>
+#include <maya/MSelectionList.h>
+#include <maya/MGlobal.h>
+#include <maya/MItSelectionList.h>
+#include <maya/MFnMesh.h>
+#include <maya/MItMeshEdge.h>
+
+
