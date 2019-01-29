e95a66769b12092a82ce915452c6864eeb87dcd9
Jason <kingmax_res@163.com>
2019-01-30 00:26:46 +0800

    + SelectRingContext1

---
+++ ch11_contexts_tools/SelectRingContext1/SelectRingContext1.cpp
@@ -0,0 +1,32 @@
+#include <maya/MPxContext.h>
+#include <maya/MGlobal.h>
+#include <maya/MStatus.h>
+#include <maya/MSelectionMask.h>
+#include <maya/MEvent.h>
+#include <maya/MString.h>
+
+class SelectRingContext1 : public MPxContext
+{
+public:
+	SelectRingContext1();
+
+	virtual void toolOnSetup(MEvent &event);
+	virtual void toolOffCleanup();
+
+	virtual MStatus doPress(MEvent &event);
+	virtual MStatus doRelease(MEvent &event);
+
+private:
+	MGlobal::MSelectionMode prevSelMode;
+	MSelectionMask prevCompMask;
+	MSelectionMask prevObjMask;
+
+	MGlobal::ListAdjustment listAdjust;
+
+	short pressX, pressY;
+	short releaseX, releaseY;
+	int clickBoxSize;
+
+	static const MString helpText;
+};
+
